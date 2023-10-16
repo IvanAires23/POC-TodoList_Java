46d04aa9227f7a91edace32e6001682bf4aa63b0
Ivan <ivanaires32@gmail.com>
2023-10-16 20:26:59 -0300

    chore: Dockerfile

--- src/main/java/br/com/ivanaires/todolist/task/TaskModel.java
+++ src/main/java/br/com/ivanaires/todolist/task/TaskModel.java
@@ -29,4 +29,11 @@ public class TaskModel {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title não pode ter mais de 50 caracteries");
+        }
+        this.title = title;
+    }
 }
