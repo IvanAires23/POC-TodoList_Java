5213210327364e253cc5dec6dcfcae2ff9b6f46d
Ivan <ivanaires32@gmail.com>
2023-10-16 09:38:12 -0300

    feat: create tasks

--- src/main/java/br/com/ivanaires/todolist/task/TaksController.java
+++ src/main/java/br/com/ivanaires/todolist/task/TaksController.java
@@ -1,5 +1,23 @@
 package br.com.ivanaires.todolist.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaksController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(200).body(task);
+    }
 
 }
