1f968e31ee06e4ffdb556a6c923bcc2de36035cf
Ivan <ivanaires32@gmail.com>
2023-10-16 12:05:16 -0300

    feat: find tasks by user

--- src/main/java/br/com/ivanaires/todolist/task/TaksController.java
+++ src/main/java/br/com/ivanaires/todolist/task/TaksController.java
@@ -1,11 +1,13 @@
 package br.com.ivanaires.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,8 @@ public class TaksController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        taskModel.setUserId((UUID) request.getAttribute("userId"));
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID) userId);
         var task = this.taskRepository.save(taskModel);
 
         var currentDate = LocalDateTime.now();
@@ -34,6 +37,13 @@ public class TaksController {
                     .body("Data de inicio da tarefa deve ser menor que a data de termino");
         }
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+        return tasks;
     }
 
 }
